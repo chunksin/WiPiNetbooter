ccdfe40b2f405f6c811e4d00d4c00e30561369f8
chunksin <chunksinspace@gmail.com>
2021-08-11 19:00:59 +0100

    Add files via upload

--- var/www/html/load.php
+++ var/www/html/load.php
@@ -30,9 +30,8 @@ $dimm = $_GET["dimm"];
 $mapping = $_GET["mapping"];
 $ffb = $_GET["ffb"];
 $filesize = gzfilesize($rompath);
-
-$command = escapeshellcmd('sudo python /sbin/piforce/webforce.py '.$rom.' '.$dimm.' '.$relaymode.' '.$zeromode.' '.$mapping.' '.$ffb);
-$output = shell_exec($command . '> /dev/null 2>/dev/null &');
+ini_set('output_buffering', false);
+$last = 0;
 
 echo '<p>';
 
@@ -44,44 +43,49 @@ $frames = ($totalloadtime*10);
 ?>
 
 <section><center>
-<body onload="move()">
 
 <?php
-echo '<h1>Loading<br>'.$name.'<h1></center>';
+echo '<h1>Loading<br>'.$name.'</h1></center>';
 ?>
 
 <div id="myProgress">
   <div id="myBar">0%</div>
 </div>
 
+<?php
+
+$command = escapeshellcmd('sudo python /sbin/piforce/webforce.py '.$rom.' '.$dimm.' '.$relaymode.' '.$zeromode.' '.$mapping.' '.$ffb);
+$output = shell_exec($command . '> /dev/null 2>/dev/null &');
+
+$progress = 0;
+while($progress < 100) {
+    $handle = popen('sudo tail -n 1 /var/log/progress.txt', 'r');
+    $progress = fgets($handle);
+    if(($progress > $last && $progress < 100) || $progress == 10){
+         echo '<script>';
+         echo 'var elem = document.getElementById("myBar");';
+         echo 'elem.style.width = '.$progress.' + "%";';
+         echo 'elem.innerHTML = '.$progress.'  + "%";';
+         echo '</script>';
+    }
+    $last = $progress;
+    ob_flush(); 
+    flush();
+    sleep(0.1);
+    pclose($handle);
+}
+
+
+echo '<script>';
+echo 'var elem = document.getElementById("myBar");';
+echo 'elem.style.width = 100 + "%";';
+echo 'elem.innerHTML = 100  + "%";';
+echo '</script>';
+echo '<br><center><a href="gamelist.php?display=all#anchor'.$name.'">LOADING COMPLETE</a></center>';
+?>
+
 <script type="text/javascript">
 <?php
-echo 'setTimeout(function(){window.location="gamelist.php?display=all#anchor'.$name.'";}, '.$redirecttimer.')';
+echo 'setTimeout(function(){window.location="gamelist.php?display=all#anchor'.$name.'";}, 2000)';
 ?>
-</script>
-
-<script>
-var i = 0;
-function move() {
-  if (i == 0) {
-    i = 1;
-    var elem = document.getElementById("myBar");
-    var width = 0;
-
-<?php
-echo 'var id = setInterval(frame, '.$frames.');';
-?>
-
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-        i = 0;
-      } else {
-        width++;
-        elem.style.width = width + "%";
-        elem.innerHTML = width  + "%";
-      }
-    }
-  }
-}
 </script>
